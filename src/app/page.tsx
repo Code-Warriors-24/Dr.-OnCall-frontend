59add702d2691874d16f0bc219d9e3d155c196ea
NurulIslam-EEE <nurul.islam02310@gmail.com>
2024-07-11 16:30:07 +0600

    local storage utils function and constant added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,16 @@ export default function Home() {
   const count = useAppSelector((state) => state.counter.value);
   const dispatch = useAppDispatch();
 
-  console.log("toolkit", count);
+  // console.log("toolkit", count);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center p-24">
       <h1>{count}</h1>
-      <button onClick={() => dispatch(increment())}>Add </button>
+      <button
+        className="hover:border-blue-100"
+        onClick={() => dispatch(increment())}
+      >
+        Add{" "}
+      </button>
       <button onClick={() => dispatch(decrement())}>Minus </button>
     </main>
   );
