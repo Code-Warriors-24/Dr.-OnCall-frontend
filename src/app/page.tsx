ce65a427abc4da2c782dad68eda67f1298d1b6f0
NurulIslam-EEE <nurul.islam02310@gmail.com>
2024-07-15 21:40:50 +0600

    component folder added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,22 +2,35 @@
 import { decrement, increment } from "@/redux/features/counterSlice";
 import { useAppDispatch, useAppSelector } from "@/redux/hooks";
 import Image from "next/image";
+import { useEffect, useRef } from "react";
 
 export default function Home() {
-  const count = useAppSelector((state) => state.counter.value);
+  const count = useAppSelector((state) => state.counter);
   const dispatch = useAppDispatch();
 
-  // console.log("toolkit", count);
+  const btnRef = useRef();
+
+  useEffect(() => {
+    console.log("cccc", count);
+  }, []);
+
+  console.log("toolkit");
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
-      <h1>{count}</h1>
+      <h1 className="text-orange-500 font-bold text-2xl">{count.value}</h1>
       <button
-        className="hover:border-blue-100"
+        className="px-5 border border-amber-600"
         onClick={() => dispatch(increment())}
       >
         Add{" "}
       </button>
-      <button onClick={() => dispatch(decrement())}>Minus </button>
+
+      <button
+        className="px-5 my-2 border border-amber-600"
+        onClick={() => dispatch(decrement())}
+      >
+        Minus{" "}
+      </button>
     </main>
   );
 }
